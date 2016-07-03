12bcaaa927e0d9a15460c4774f6737da07d00368
Alexey Malyshev <alexey.malyshev@dominionmarinemedia.com>
2016-07-02 20:30:03 -0700

    Add minification.

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,16 @@ module.exports = {
   module: {
     preLoaders: [
       {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
     ]
   },
   plugins: [
